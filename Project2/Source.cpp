efa0894290e4ad7c42aff56617d4331ca0f14792
StanislavSorochak <75738611+StanislavSorochak@users.noreply.github.com>
2020-12-09 18:48:10 +0200

    first branch_02 commit

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -12,16 +12,16 @@ using namespace std;
 int main()
 {
 	double a; // вхідний параметр
-	double z1; // результат обчислення 1-го виразу
-	// double z2; // результат обчислення 2-го виразу
+	// double z1; // результат обчислення 1-го виразу
+	double z2; // результат обчислення 2-го виразу
 
 	cout << "x = "; cin >> a;
 
-	z1 = (sin(a) + sin(5.0 * a) - sin(3.0 * a)) / (cos(a) - cos(3.0 * a) + cos(5.0 * a));
-	// z2 = tan(3.0 * a);
+	// z1 = (sin(a) + sin(5.0 * a) - sin(3.0 * a)) / (cos(a) - cos(3.0 * a) + cos(5.0 * a));
+	z2 = tan(3.0 * a);
 
-	cout << "z1 = " << z1 << endl;
-	// cout << "z2 = " << z2 << endl;
+	// cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
 
 	cin.get();
 	return 0;
